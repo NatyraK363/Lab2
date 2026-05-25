8cdaab576fd3f50a8de07768b35ab92c3fe96273
NatyraK363 <nk67363@ubt-uni.net>
2026-05-25 21:25:38 +0200

    configure MySQL db

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <h2 className="text-2xl font-semibold">Login</h2>
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,5 @@
+function Register() {
+  return <h2 className="text-2xl font-semibold">Register</h2>
+}
+
+export default Register
